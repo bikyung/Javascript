cd38c1de13929060767222705ef55b0c692759d4
haemun Lee <lhmun1129@daum.net>
2021-11-18 11:20:51 +0900

    자바스크립트 변수 기초

---
+++ test1.js
@@ -0,0 +1,26 @@
+//! 변수
+
+//! 재사용이 가능(let)
+
+//* 변수 선언!
+let a = 2;
+let b = 5;
+
+console.log(a + b) //? 7
+console.log(a - b) //? -3
+console.log(a * b) //? 10
+console.log(a / b) //? 0.4
+
+//* 값(데이터)의 재할당 가능!
+let a = 12;
+console.log(a); //? 12
+
+a = 999;
+console.log(a); //? 999
+
+//* 값(데이터)의 재할당 불가! (const)
+const a = 12;
+console.log(a); //? 12
+
+a = 999;
+console.log(a); //? TypeError 발생
