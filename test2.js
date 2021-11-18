868d6dfa0e627204f60db862a028a8838bd49133
haemun Lee <lhmun1129@daum.net>
2021-11-18 14:30:59 +0900

    자바스크립트 함수 기초(기명함수,익명함수,객체)

--- test2.js
+++ test2.js
@@ -15,9 +15,9 @@ function returnFunc() {
   return 123;
 }
 
-let a = returnFunc();
+// let a = returnFunc();
 
-console.log(a) //? 123
+// console.log(a) //? 123
 
 //* 함수 선언!
 
@@ -31,3 +31,33 @@ let b = sum(7, 12);
 let c = sum(2, 4);
 
 console.log(a, b, c); //? 3, 19, 6
+
+//* 기명(이름이있는)함수
+//* 함수선언식
+function hello() {
+  console.log('Hello~');
+}
+
+//* 익명(이름이 없는)함수
+//* 함수 표현식!
+let world = function() {
+  console.log('World~');
+}
+//* 함수 호출
+hello(); //? Hello~
+world(); //? World~
+
+// * 객체 데이터
+const hero = {
+  name: 'Thor',
+  age: 36,
+  //* 매소드(Method)
+  getName: function() {
+    return this.name;
+  }
+}
+
+const hisName = hero.getName();
+console.log(hisName); //? Thor
+//* 또는
+console.log(hero.getName()); //? Thor
