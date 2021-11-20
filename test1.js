1c52974da31ea23f16a0bdfeaed6817b300790b0
haemun Lee <lhmun1129@daum.net>
2021-11-20 12:34:55 +0900

    자바스크립트 변수 기초( 전역, 지역)

--- test1.js
+++ test1.js
@@ -1,26 +1,43 @@
-//! 변수
+// //! 변수
 
-//! 재사용이 가능(let)
+// //* 변수 선언 방법!
+// let a = 2;
+// let b = 5;
 
-//* 변수 선언!
-let a = 2;
-let b = 5;
+// console.log(a + b) //? 결과값 :7
+// console.log(a - b) //? 결과값 :-3
+// console.log(a * b) //? 결과값 :10
+// console.log(a / b) //? 결과값 :0.4
 
-console.log(a + b) //? 7
-console.log(a - b) //? -3
-console.log(a * b) //? 10
-console.log(a / b) //? 0.4
+// //* 값(데이터)의 재할당 가능! (let)
+// let a = 12;
+// console.log(a); //? 결과값 : 12
 
-//* 값(데이터)의 재할당 가능! (let)
-let a = 12;
-console.log(a); //? 12
+// a = 999;
+// console.log(a); //? 결과값 :999
 
-a = 999;
-console.log(a); //? 999
+// //* 값(데이터)의 재할당 불가! (const)
+// const a = 12;
+// console.log(a); //? 결과값 : 12
 
-//* 값(데이터)의 재할당 불가! (const)
-const a = 12;
-console.log(a); //? 12
+// a = 999;
+// console.log(a); //? TypeError 발생
 
-a = 999;
-console.log(a); //? TypeError 발생
+// // ! 변수를 선언할 때 주의할점
+// let value = 1;
+// let value = 1;
+
+// let global = '전역'
+
+// if (global === '전역') {
+//   let global = '지역'
+
+//   console.log(gloabal); //? 결과값: 지역
+// }
+
+// console.log(global); //? 결과값: 전역
+
+
+
+  let value = 1;
+  value = 2;
